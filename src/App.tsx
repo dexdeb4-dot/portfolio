cb4ac49de74163958a0e71a3b40b53d517ce7cf7
dexdeb4-dot <dexdeb4@gmail.com>
2026-06-19 13:50:30 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -19,6 +19,7 @@ import SplashCursor from './components/SplashCursor';
 import Aurora from './components/Aurora';
 import { Laptop3D } from './components/Laptop3D';
 import MagnetLines from './components/MagnetLines';
+// @ts-ignore
 import AdminDashboard from './AdminDashboard';
 gsap.registerPlugin(ScrollTrigger);
 
